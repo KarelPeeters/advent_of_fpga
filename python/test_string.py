0e0d28c564a6157ce9fb7909f51d320f8f1e2e81
Karel Peeters <karel.peeters.leuven@gmail.com>
2026-01-11 16:46:34 +0100

    Move axi test utility to util.py

--- python/test_string.py
+++ python/test_string.py
@@ -1,11 +1,8 @@
-"""Test to/from string modules"""
-
 from pathlib import Path
-from typing import List, Optional, Tuple
 import hwl
 import random
 
-from util import compile_manifest
+from util import compile_manifest, send_axi_through_module
 
 
 def test_parse_points(tmp_path: Path):
@@ -41,39 +38,3 @@ def test_int_to_chars(tmp_path: Path):
     output = send_axi_through_module(inst, values, max_cycles=2 * len(expected_output_string) + 16)
     output_string = "".join(chr(x) for x in output)
     assert output_string == expected_output_string
-
-
-def send_axi_through_module(inst: hwl.VerilatedInstance, input_data: List, max_cycles: int) -> List:
-    output = []
-
-    # reset
-    ports = inst.ports
-    ports.rst.value = True
-    inst.step(1)
-    ports.rst.value = False
-    inst.step(1)
-
-    # clock loop
-    for i in range(max_cycles):
-        ports.clk.value = True
-        inst.step(1)
-        ports.clk.value = False
-        inst.step(1)
-
-        # handle precious input handshake
-        if ports.input_valid.value and ports.input_ready.value:
-            ports.input_valid.value = False
-
-        # send input
-        if not ports.input_valid.value and input_data:
-            ports.input_valid.value = True
-            ports.input_data.value = input_data[0]
-            input_data = input_data[1:]
-
-        # handle output handshake, collect output
-        if ports.output_valid.value and ports.output_ready.value:
-            output.append(ports.output_data.value)
-        ports.output_ready.value = True
-
-    # assert not input_data, "Failed to send all input"
-    return output
