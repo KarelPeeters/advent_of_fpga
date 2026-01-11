6f5d9b63cba083bce3866fb4eafe3ba8e087be51
Karel Peeters <karel.peeters.leuven@gmail.com>
2026-01-11 15:50:09 +0100

    Implement int_to_chars module

--- python/test_string.py
+++ python/test_string.py
@@ -8,21 +8,43 @@ import random
 from util import compile_manifest
 
 
-# TODO extract common axi in/out test infrastructure
-def test_parse(tmp_path: Path):
+def test_parse_points(tmp_path: Path):
+    # TODO include some edge cases, eg. 0,0, max values, or a bunch of short pairs in sequence
     M = 10**5
     sample_count = 64
 
     c = compile_manifest()
-    m: hwl.Module = c.resolve("string.parse")(M=M)
+    m: hwl.Module = c.resolve("string.parse_points")(M=M)
     inst = m.as_verilated(tmp_path).instance()
 
     random.seed(0x42)
     samples = [(random.randrange(M), random.randrange(M)) for _ in range(sample_count)]
     input_string = "\n".join(f"{x},{y}" for x, y in samples) + "\n"
 
-    input_remaining: str = input_string
-    output_received: List[Tuple[int, int]] = []
+    output = send_axi_through_module(inst, [ord(c) for c in input_string], max_cycles=len(input_string) + 16)
+    assert output == samples
+
+
+def test_int_to_chars(tmp_path: Path):
+    # TODO include some edge cases, eg. 0-9, max value, ...
+    M = 10**5
+    sample_count = 64
+
+    c = compile_manifest()
+    m: hwl.Module = c.resolve("string.int_to_chars")(M=M)
+    inst = m.as_verilated(tmp_path).instance()
+
+    random.seed(0x42)
+    values = [random.randrange(M) for _ in range(sample_count)]
+    expected_output_string = "\n".join(str(v) for v in values) + "\n"
+
+    output = send_axi_through_module(inst, values, max_cycles=2 * len(expected_output_string) + 16)
+    output_string = "".join(chr(x) for x in output)
+    assert output_string == expected_output_string
+
+
+def send_axi_through_module(inst: hwl.VerilatedInstance, input_data: List, max_cycles: int) -> List:
+    output = []
 
     # reset
     ports = inst.ports
@@ -32,7 +54,7 @@ def test_parse(tmp_path: Path):
     inst.step(1)
 
     # clock loop
-    for i in range(len(input_string) + 16):
+    for i in range(max_cycles):
         ports.clk.value = True
         inst.step(1)
         ports.clk.value = False
@@ -42,16 +64,16 @@ def test_parse(tmp_path: Path):
         if ports.input_valid.value and ports.input_ready.value:
             ports.input_valid.value = False
 
-        # send input, with trailing zero to signal end
-        if not ports.input_valid.value and input_remaining:
+        # send input
+        if not ports.input_valid.value and input_data:
             ports.input_valid.value = True
-            ports.input_data.value = ord(input_remaining[0])
-            input_remaining = input_remaining[1:]
+            ports.input_data.value = input_data[0]
+            input_data = input_data[1:]
 
         # handle output handshake, collect output
         if ports.output_valid.value and ports.output_ready.value:
-            output_received.append(ports.output_data.value)
+            output.append(ports.output_data.value)
         ports.output_ready.value = True
 
-    assert input_remaining == "", "Failed to send all input"
-    assert output_received == samples, "Incorrect output"
+    # assert not input_data, "Failed to send all input"
+    return output
