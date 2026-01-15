254f5a754bdde9475259716048ab2cfb46ec1551
Karel Peeters <karel.peeters.leuven@gmail.com>
2026-01-15 02:26:27 +0100

    Scale core to 16x16, adding lots of pipelining along the way

--- python/compile.py
+++ python/compile.py
@@ -4,8 +4,18 @@ from util import compile_manifest
 
 
 def main() -> None:
+    MAX_POINT_COUNT = 2 * (1024 * 4) * 64
+    COORD_LIMIT = 10**5
+    RAM_PIPELINE = 8
+    CORE_WIDTH = 16
+
     c = compile_manifest()
-    top: hwl.Module = c.resolve("top.top")
+    top: hwl.Module = c.resolve("top.top_generic")(
+        MAX_POINT_COUNT=MAX_POINT_COUNT,
+        COORD_LIMIT=COORD_LIMIT,
+        RAM_PIPELINE=RAM_PIPELINE,
+        CORE_WIDTH=CORE_WIDTH,
+    )
 
     out_dir = Path(__file__).parent / "../output"
     out_dir.mkdir(exist_ok=True, parents=True)
--- python/test_top.py
+++ python/test_top.py
@@ -6,24 +6,29 @@ from typing import List, Tuple
 import hwl
 import random
 
+import pytest
+
 from util import EXTRA_VERILOG_FILES, compile_manifest, send_axi_through_module
 
 
 # TODO test some edge cases, eg. 0,0, max values, or a bunch of short pairs in sequence
 
 
-def test_top_single(tmp_path: Path):
-    inst = top_instance(tmp_path)
+@pytest.mark.parametrize("core_width", [1, 2, 3])
+def test_top_single(tmp_path: Path, core_width: int):
+    inst = top_instance(tmp_path, core_width=core_width)
     assert run_and_check_top(inst, [(16, 32)]) == 1
 
 
-def test_top_double(tmp_path: Path):
-    inst = top_instance(tmp_path)
+@pytest.mark.parametrize("core_width", [1, 2, 3])
+def test_top_double(tmp_path: Path, core_width: int):
+    inst = top_instance(tmp_path, core_width=core_width)
     assert run_and_check_top(inst, [(16, 32), (20, 30)]) == 15
 
 
-def test_top_short(tmp_path: Path):
-    inst = top_instance(tmp_path)
+@pytest.mark.parametrize("core_width", [1, 2, 3])
+def test_top_short(tmp_path: Path, core_width: int):
+    inst = top_instance(tmp_path, core_width=core_width)
 
     M = 10**5
     random.seed(0x42)
@@ -33,14 +38,15 @@ def test_top_short(tmp_path: Path):
         run_and_check_top(inst, samples)
 
 
-def test_top_random_long(tmp_path: Path):
+@pytest.mark.parametrize("core_width", [1, 8])
+def test_top_random_long(tmp_path: Path, core_width: int):
     M = 10**5
     sample_count = 64
 
     random.seed(0x42)
     samples = [(random.randrange(M), random.randrange(M)) for _ in range(sample_count)]
 
-    inst = top_instance(tmp_path)
+    inst = top_instance(tmp_path, core_width=core_width)
     run_and_check_top(inst, samples)
 
 
@@ -54,7 +60,9 @@ def run_and_check_top(inst: hwl.VerilatedInstance, samples: List[Tuple[int, int]
     expected_output_str = str(expected_output) + "\n"
 
     max_cycles = 6 * len(input_string) + len(samples) ** 2 // 2 + 512
-    output = send_axi_through_module(inst, [ord(c) for c in input_string], max_cycles=max_cycles, output_end=ord("\n"))
+    output = send_axi_through_module(
+        inst, [ord(c) for c in input_string], max_cycles=max_cycles, output_end=ord("\n")
+    )
 
     output_str = "".join(chr(c) for c in output)
     assert output_str == expected_output_str
@@ -62,7 +70,12 @@ def run_and_check_top(inst: hwl.VerilatedInstance, samples: List[Tuple[int, int]
     return int(output_str.strip())
 
 
-def top_instance(tmp_path: Path) -> hwl.VerilatedInstance:
+def top_instance(tmp_path: Path, core_width: int) -> hwl.VerilatedInstance:
     c = compile_manifest()
-    m: hwl.Module = c.resolve("top.top")
+    m: hwl.Module = c.resolve("top.top_generic")(
+        MAX_POINT_COUNT=1024,
+        COORD_LIMIT=10**5,
+        RAM_PIPELINE=8,
+        CORE_WIDTH=core_width,
+    )
     return m.as_verilated(tmp_path, extra_verilog_files=EXTRA_VERILOG_FILES).instance()
